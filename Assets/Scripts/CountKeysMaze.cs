87e334628520509472612bc11c5c4542526f6fa1
Anik Singha <aniksingha@outlook.com>
2023-11-27 19:23:24 -0500

    write key count script

---
+++ Assets/Scripts/CountKeysMaze.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CountKeysMaze : MonoBehaviour
+{
+    public TextMeshProUGUI keyCountText; // Reference to your TextMeshPro object
+    int activeKeyCount;
+
+    void Update()
+    {
+        activeKeyCount = CountActiveKeys();
+        UpdateKeyCountText();
+    }
+
+    int CountActiveKeys()
+    {
+        GameObject[] propsKeyObjects = GameObject.FindGameObjectsWithTag("Collectible");
+
+        int activeKeys = 0;
+
+        foreach (GameObject keyObject in propsKeyObjects)
+        {
+            if (keyObject.activeSelf)
+            {
+                activeKeys++;
+            }
+        }
+
+        return activeKeys;
+    }
+
+    void UpdateKeyCountText()
+    {
+        // Update the TextMeshPro text with the count of active keys
+        if (activeKeyCount != 0)
+        {
+            keyCountText.text = "Keys Left to Collect: " + activeKeyCount;
+        } else {
+            keyCountText.text = "All Keys Collected! Head to The Exit";
+        }
+    }
+}
