5df2dc4e2e27f324a4b9152dc49ed6a0ece4921a
Adrian <mysliwiec2003@gmail.com>
2023-12-03 12:26:50 -0500

    monster added

--- Assets/Scripts/Collectible.cs
+++ Assets/Scripts/Collectible.cs
@@ -7,6 +7,8 @@ public class Collectible : MonoBehaviour
 
     // Update is called once per frame
     public static event Action OnCollected;
+
+    [SerializeField] private AudioSource collection;
     void Update()
     {
         transform.localRotation = Quaternion.Euler(90f, Time.time * 100f, 0);
@@ -16,6 +18,7 @@ public class Collectible : MonoBehaviour
     {
         if(other.CompareTag("Player"))
         {
+            collection.Play();
             OnCollected?.Invoke();
             gameObject.SetActive(false);
         }
